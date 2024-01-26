ce9ce57be12c61bac0124e9b29867b14fad9ff9c
cyrej17 <49009364+cyrej17@users.noreply.github.com>
2024-01-26 19:35:04 +0800

    Update main.tf

--- terraform-cloud-aws-vpc/my_main_dir/main.tf
+++ terraform-cloud-aws-vpc/my_main_dir/main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
 module "my_vpc" {
-  source               = "../module/terraform-cloud-aws-vpc"
+  source               = "../terraform-cloud-aws-vpc/module"
   vpc_name             = "my-vpc"
   vpc_cidr_block       = "10.0.0.0/16"
   subnet_cidr_blocks   = ["10.0.1.0/24", "10.0.2.0/24"]
